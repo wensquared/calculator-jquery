c5512eaa2d30db40710067cc708dda29812c7aca
wensquared <71566436+wensquared@users.noreply.github.com>
2021-07-16 12:11:05 +0200

    added css grid

--- calc.js
+++ calc.js
@@ -1,25 +1,29 @@
 'use strict'; 
 
 var expressionArray = [];
-var tmpStr = '';
 var lastSymbol = '';
+var strExpr = '';
 
 function calculate(exprArr) {
     lastSymbol = exprArr.pop();
     let firstNum = parseFloat(exprArr[0]);
     let symbol = exprArr[1];
     let secondNum = parseFloat(exprArr[2]);
-    let result = 0;
+    let result = null;
     
     switch (symbol) {
         case '+':
             result = firstNum + secondNum;
             break;
+        
+        case '/':
+            result = firstNum / secondNum;
+            break;
     
         default:
             break;
     }
-
+    //preparing for continuous calculation
     if (lastSymbol == '=') {
         expressionArray = [];
         expressionArray.push(result);
@@ -36,23 +40,34 @@ function calculate(exprArr) {
 
 $( document ).ready(function() {
     console.log('Page loaded');
-
+    let tmpStr = '';
     $('span div').on('click', function() {
-        // console.log('Clicked number');
-        // event.preventDefault(); 
-        // $(this).css('backgroundColor','green');
+        
         let val = $(this).html();
-
-        if((val != '+') && (val != '=')) {
+        strExpr += val;
+        if((val != '+') && 
+            (val != '=') &&
+            (val != '/')
+            ) {
             tmpStr += val;
             console.log(expressionArray);
         } 
         else {
-            if (!tmpStr) {
+            // continuous calculation (calculating by adding next math operation symbol to previous result)
+            if (!tmpStr && expressionArray[0]) {
                 expressionArray.push(val);
+                strExpr = expressionArray[0] + val;
             }
+            
             else {
-                expressionArray.push(tmpStr);
+                // no continous calculation = overriding old result
+                if (expressionArray[0] && (!expressionArray[1])) {
+                    expressionArray[0] = tmpStr;
+                }
+                // continuous calculation (result with math operation symbol, adding second number term)
+                else {
+                    expressionArray.push(tmpStr);
+                }
                 expressionArray.push(val);
             }
             tmpStr = '';
@@ -60,11 +75,37 @@ $( document ).ready(function() {
         }
         
         console.log(val);
-        $('#result').html(expressionArray.join(''));
+        $('#result').html(strExpr);
 
         if(expressionArray.length == 4) {
-            let result = calculate(expressionArray);
-            console.log(result);
+            let regex = /^[\-]?\d+(\.\d+)?[\+\-\*\/]{1}[\+\-]?\d+(\.\d+)?[\+\-\*\/\=]{1}$/;
+            let divideZero = /^[\-]?\d+(\.\d+)?\/{1}[\+\-]?0\=$/;
+            if (!regex.test(expressionArray.join(''))){
+                console.log('no valid expression');
+                expressionArray = [];
+                strExpr = '';
+                // $('#result').html(strExpr);
+            }
+            else if (divideZero.test(expressionArray.join(''))) {
+                console.log('errrrr');
+                expressionArray = [];
+                strExpr = '';
+                // $('#result').html(strExpr);
+            }
+            else {
+                let result = calculate(expressionArray);
+                $('#result').html(result);
+                strExpr = '';
+                console.log(result);
+            }
+            
+            //console.log(result);
+        }
+        if(expressionArray.length > 4) {
+            console.log('too long');
+            expressionArray = [];
+            strExpr = '';
+            $('#result').html(strExpr);
         }
 
     })
